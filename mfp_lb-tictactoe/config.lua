328b49669fe34b3e04af1d81158f7be8687cfb5d
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2024-03-18 10:42:34 +0100

    Update config.lua

--- mfp_lb-tictactoe/config.lua
+++ mfp_lb-tictactoe/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
-Config.name = "Tic Tac Toe" -- Name of the App
-Config.description = "X your Life" -- disc in the appstore (edit for your local)
+Config.name = "TicTacToe" -- Name of the App
+Config.description = "Classic TicTacToe game for everyone" -- disc in the appstore (edit for your local)
 Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
 Config.size = 2042 -- OPTIONAL in kb
 Config.price = 50 -- OPTIONAL, Make players pay with in-game money to download the app. Do 0 for it to be free.
